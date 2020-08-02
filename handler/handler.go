a9b401d8c66dc704e76d2cc4b80d37c2996d2f97
abuelhassan <abuelhassan93@gmail.com>
2020-08-02 08:30:23 +0200

    add HealthCheck and NotFound handlers

---
+++ handler/handler.go
@@ -0,0 +1,16 @@
+package handler
+
+import "net/http"
+
+func HealthCheck(w http.ResponseWriter, _ *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	// In the future we could report back on the status of our DB, or our cache
+	// (e.g. Redis) by performing a simple PING, and include them in the response.
+	_, _ = w.Write([]byte(`{"alive":true}`))
+}
+
+func NotFound(w http.ResponseWriter, _ *http.Request) {
+	w.WriteHeader(http.StatusNotFound)
+}
